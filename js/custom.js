c408da13f24b55bd34ae7b5f8db61526f1e7d4ae
Muhammad Idrees <idreesiqbal104@gmail.com>
2019-04-01 00:09:16 -0700

    ui partially done for parallels landing page

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+// alert('hello idrees');
+
+function clickFun(){
+    alert('hello idrees');
+}
+
+function changeColor(){
+    document.getElementById('changecolor').style.color = 'red';
+}
+
+function add(a, b){
+    var x = a+b ;
+    console.log(x);
+}   
+
+function changeRed(){
+    document.getElementById('redClass').classList.add('sampleIdreesClass');
+}
